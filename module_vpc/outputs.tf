849a4e4fcec0c4cf3cbc783e5ff7d165bfe4444c
Mateusz Małkiewicz <41550579+MateuszMalkiewicz@users.noreply.github.com>
2022-10-10 16:48:08 +0200

    Update outputs.tf

--- module_vpc/outputs.tf
+++ module_vpc/outputs.tf
@@ -3,7 +3,7 @@ output "subnet_priv_ids" {
 }
 
 output "subnet_pub_ids" {
-  value = { for k, v in aws_subnet.thhiis : k => v.id }
+  value = { for k, v in aws_subnet.thhiis : k => v.id }  # huuuuh? I bet a typo came in :D
 }
 
 output "subnets_priv" {
@@ -11,5 +11,10 @@ output "subnets_priv" {
 }
 
 output "subnets_pub" {
-  value = values(aws_subnet.thhiis)[*].id
+  value = values(aws_subnet.thhiis)[*].id  # rule of thumb: disconnect yourself from the code you write and treat it as written by random person and ask youself how much sense does this make
+  # I try to do this as much as I can, it helps a lot, cause I'm not the sharpest tool in a shed
+  # She was looking kind of dumb
+  # with her finger and her thumb
+  # In the shape of an "L"
+  # on her forehead
 }
